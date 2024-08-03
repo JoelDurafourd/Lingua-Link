25f4eabbfecc04405352fdf2394bc0428c25032a
Joel Durafourd <joel.durafourd@gmail.com>
2024-08-03 15:49:36 +0900

    created a lot of great bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,7 @@
 class PagesController < ApplicationController
-  include Pundit
-    skip_after_action :verify_authorized, only: [:home]
-
-    skip_before_action :authenticate_user!, only: [:home]
-
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
   def home
-  end
-
-  # even though there is no defined index, removing this line will cause the website to break because pundit is expecting an index in the pages controller.
-  # DO NOT REMOVE unless you are prepared to code out the pundit except in ApplicationController
-  def index
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    # references the user in the params, NOT NECESSARILY THE CURRENT USER
+    # current user will be referenced by current_user, not @user
+    @users = User.all
+  end
+
+  def show
+    # user profile page, can be any user referenced not just current user, see above comments
+    authorize @user
+    @user = User.find(params[:id])
+  end
+
+  def dashboard
+    @user = current_user
+  end
+end
