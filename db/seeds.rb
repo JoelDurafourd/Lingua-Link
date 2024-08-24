6c87519c67742ff235a2f22fd6c1bc8da291e164
Nakia Wilson <n.wil@workmail.com>
2024-08-24 16:54:34 +0900

    fixed width of profiles and notes section. added images to clients. changed avatar size and shape.

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,35 +48,43 @@ puts "Created #{user5}!"
 
 
 client1 = Client.create!(lineid: "ABC1234", phone_number: "555-0101", name: "Alice Johnson", nickname: "AliceJ1")
-client3.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389003/download_xpmy4i.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
+client1.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721388471/jane_smith_jf19ib.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client1}!"
 
 client2 = Client.create!(lineid: "XYZ5678", phone_number: "555-0102", name: "Bob Smith", nickname: "BobS2")
-client1.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389003/download_xpmy4i.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
+client2.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389003/download_xpmy4i.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client2}!"
 
 client3 = Client.create!(lineid: "LMN9101", phone_number: "555-0103", name: "Charlie Brown", nickname: "CharlieB3")
+client3.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389002/download_17_qasdmi.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client3}!"
 
 client4 = Client.create!(lineid: "DEF2345", phone_number: "555-0104", name: "Diana Ross", nickname: "DianaR4")
+client4.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389003/download_19_eeri0z.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client4}!"
 
 client5 = Client.create!(lineid: "GHI6789", phone_number: "555-0105", name: "Edward White", nickname: "EdwardW5")
+client5.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389001/download_13_ach1fx.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client5}!"
 
 client6 = Client.create!(lineid: "JKL0123", phone_number: "555-0106", name: "Fiona Green", nickname: "FionaG6")
+client6.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389001/download_12_s3mq6f.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client6}!"
 
 client7 = Client.create!(lineid: "MNO4567", phone_number: "555-0107", name: "George Black", nickname: "GeorgeB7")
+client7.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721388991/download_10_osrm3v.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client7}!"
 
 client8 = Client.create!(lineid: "PQR8901", phone_number: "555-0108", name: "Hannah Blue", nickname: "HannahB8")
+client8.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389002/download_15_scg5fl.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client8}!"
 
 client9 = Client.create!(lineid: "STU2345", phone_number: "555-0109", name: "Ian Gray", nickname: "IanG9")
+client9.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389002/download_14_ppqbeb.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client9}!"
 
 client10 = Client.create!(lineid: "VWX6789", phone_number: "555-0110", name: "Jane White", nickname: "JaneW10")
+client10.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721388990/download_7_xu1g2a.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
 puts "Created #{client10}!"
 
 # Joel's seeds
@@ -349,41 +357,137 @@ booking10 = Booking.create!(
 )
 puts "Created #{booking10}!"
 
+# emilie's seeds
 
+start_date = Date.new(2024, 8, 26) # Monday of the week
+end_date = start_date + 4 # Friday of the week
 
+# Loop through each day from Monday to Friday
+(start_date..end_date).each do |date|
+  # Create morning shift availability
+  morning_availability = Availability.create!(
+    start_time: DateTime.new(date.year, date.month, date.day, 8, 0, 0),
+    end_time: DateTime.new(date.year, date.month, date.day, 12, 0, 0),
+    user_id: nakia.id
+  )
+  puts "Created morning availability for #{date.strftime('%Y-%m-%d')}!"
 
-# user2 = User.create!(email: "jane.smith@email.com", password: "password123", first_name: "Jane", last_name: "Smith", description: "Passionate about cooking and exploring new recipes.")
-# user2.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721388471/jane_smith_jf19ib.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user2}!"
+  # Create afternoon shift availability
+  afternoon_availability = Availability.create!(
+    start_time: DateTime.new(date.year, date.month, date.day, 13, 0, 0),
+    end_time: DateTime.new(date.year, date.month, date.day, 18, 0, 0),
+    user_id: nakia.id
+  )
+  puts "Created afternoon availability for #{date.strftime('%Y-%m-%d')}!"
+end
 
-# user3 = User.create!(email: "michael.jones@email.com", password: "securepass", first_name: "Michael", last_name: "Jones", description: "Tech enthusiast and avid gamer.")
-# user3.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389002/download_17_qasdmi.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user3}!"
+# Create Bookings
+booking1 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 26, 8, 0, 0), # Example time slot within the availability
+  end_time: DateTime.new(2024, 8, 26, 9, 0, 0),
+  status: 1, # Assuming 1 corresponds to a status like 'confirmed'
+  user_id: nakia.id,
+  client_id: client1.id,
+  title: 'Consultation with Alice',
+  description: 'Discussing project details with Alice Johnson.'
+)
+puts "Created #{booking1}!"
 
-# user4 = User.create!(email: "emily.wilson@email.com", password: "p@ssw0rd", first_name: "Emily", last_name: "Wilson", description: "Fitness coach helping people achieve their goals.")
-# user4.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389003/download_19_eeri0z.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user4}!"
+booking2 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 26, 9, 0, 0), # Example time slot within the availability
+  end_time: DateTime.new(2024, 8, 26, 10, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client2.id,
+  title: 'Consultation with Bob',
+  description: 'Discussion with Bob Smith about upcoming tasks.'
+)
+puts "Created #{booking2}!"
 
-# user5 = User.create!(email: "david.brown@email.com", password: "david123", first_name: "David", last_name: "Brown", description: "Travel blogger exploring hidden gems around the world.")
-# user5.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389001/download_13_ach1fx.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user5}!"
+booking3 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 28, 10, 0, 0), # Example time slot within the availability
+  end_time: DateTime.new(2024, 8, 28, 11, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client3.id,
+  title: 'Consultation with Charlie',
+  description: 'Meeting with Charlie Brown to review progress.'
+)
+puts "Created #{booking3}!"
 
-# user6 = User.create!(email: "anna.johnson@email.com", password: "password456", first_name: "Anna", last_name: "Johnson", description: "Art enthusiast and painter.")
-# user6.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389001/download_12_s3mq6f.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user6}!"
+booking4 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 29, 13, 0, 0), # Example time slot within the availability
+  end_time: DateTime.new(2024, 8, 29, 14, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client4.id,
+  title: 'Consultation with Diana',
+  description: 'Discussing contract terms with Diana Ross.'
+)
+puts "Created #{booking4}!"
 
-# user7 = User.create!(email: "matthew.wilson@email.com", password: "securepassword", first_name: "Matthew", last_name: "Wilson", description: "Nature lover and hiking guide.")
-# user7.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721388991/download_10_osrm3v.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user7}!"
+# booking5 = Booking.create!(
+#   start_time: DateTime.new(2024, 8, 30, 14, 0, 0), # Example time slot within the availability
+#   end_time: DateTime.new(2024, 8, 30, 15, 0, 0),
+#   status: 1,
+#   user_id: nakia.id,
+#   client_id: client5.id,
+#   title: 'Consultation with Edward',
+#   description: 'Meeting with Edward White for project planning.'
+# )
+# puts "Created #{booking5}!"
 
-# user8 = User.create!(email: "sarah.davis@email.com", password: "p@ssw0rd123", first_name: "Sarah", last_name: "Davis", description: "Yoga instructor promoting wellness.")
-# user8.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389002/download_15_scg5fl.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user8}!"
+# booking6 = Booking.create!(
+#   start_time: DateTime.new(2024, 8, 30, 15, 0, 0), # Example time slot within the availability
+#   end_time: DateTime.new(2024, 8, 30, 16, 0, 0),
+#   status: 1,
+#   user_id: nakia.id,
+#   client_id: client6.id,
+#   title: 'Consultation with Fiona',
+#   description: 'Consultation with Fiona Green on upcoming deliverables.'
+# )
+# puts "Created #{booking6}!"
 
-# user9 = User.create!(email: "peter.jackson@email.com", password: "peter123", first_name: "Peter", last_name: "Jackson", description: "Photographer capturing moments.")
-# user9.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721389002/download_14_ppqbeb.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user9}!"
+booking7 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 28, 16, 0, 0), # Example time slot within the availability
+  end_time: DateTime.new(2024, 8, 28, 17, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client7.id,
+  title: 'Consultation with George',
+  description: 'Discussing project milestones with George Black.'
+)
+puts "Created #{booking7}!"
 
-# user10 = User.create!(email: "elizabeth.moore@email.com", password: "lizzie456", first_name: "Elizabeth", last_name: "Moore", description: "History buff and tour guide.")
-# user10.photo.attach(io: URI.open(Cloudinary::Utils.cloudinary_url("https://res.cloudinary.com/dxljoz6af/image/upload/v1721388990/download_7_xu1g2a.jpg", size: '300x300', format: 'png')), filename: 'avatar.png')
-# puts "Created #{user10}!"
+booking8 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 29, 17, 0, 0), # Example time slot within the availability
+  end_time: DateTime.new(2024, 8, 29, 18, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client8.id,
+  title: 'Consultation with Hannah',
+  description: 'Meeting with Hannah Blue to finalize project details.'
+)
+puts "Created #{booking8}!"
+
+booking9 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 27, 8, 0, 0), # Example time slot on the next day
+  end_time: DateTime.new(2024, 8, 27, 9, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client9.id,
+  title: 'Consultation with Ian',
+  description: 'Consultation with Ian Gray to review project status.'
+)
+puts "Created #{booking9}!"
+
+booking10 = Booking.create!(
+  start_time: DateTime.new(2024, 8, 27, 9, 0, 0), # Example time slot on the next day
+  end_time: DateTime.new(2024, 8, 27, 10, 0, 0),
+  status: 1,
+  user_id: nakia.id,
+  client_id: client10.id,
+  title: 'Consultation with Jane',
+  description: 'Meeting with Jane White to discuss project goals.'
+)
+puts "Created #{booking10}!"
